b07c03abc3c1804155cd8a9c7186ee391b26e81f
Ojas Chauhan <ojaschauhan44@gmail.com>
2020-07-09 19:15:17 +0530

    first

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,45 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Magicn(),
       ),
     );
+
+class Magicn extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black26,
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: Text('Magic 8 ball'),
+      ),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int a = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: FlatButton(
+        onPressed: () {
+          setState(() {
+            a = Random().nextInt(5) + 1;
+            print(a);
+          });
+        },
+        child: Image.asset('images/ball$a.png'),
+      ),
+    );
+  }
+}
